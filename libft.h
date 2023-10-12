326e0fd3449d096368ddea7c196a784c34442c89
Giulia Maia Perrone <gmaia-pe@c1r8s14.42porto.com>
2023-10-12 19:12:26 +0100

    more functions with norm

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: gmaia-pe <gmaia-pe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 21:09:16 by gmaia-pe          #+#    #+#             */
-/*   Updated: 2023/10/12 16:14:55 by gmaia-pe         ###   ########.fr       */
+/*   Updated: 2023/10/12 17:20:12 by gmaia-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,5 +43,6 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	*ft_itoa(int n);
 
 #endif
